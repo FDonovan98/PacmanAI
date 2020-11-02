ec250e505d9d3421d7e1aed3daf01d72ea91a91d
Walton-Rivers <Joseph.WaltonRivers@falmouth.ac.uk>
2020-11-02 22:16:42 +0000

    initial commit for unity pacman

---
+++ Assets/Scripts/PacmanController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PacmanController : MonoBehaviour
+{
+    public const float POWER_PILL_TIMER = 10;
+    public const int PILL_SCORE = 10;
+    public const int GHOST_SCORE = 100;
+    public const float SPEED_FACTOR = 2.5F;
+
+    private Rigidbody body;
+    private GameController game;
+    public float invulnTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+        game = GameObject.Find("GameController").GetComponent<GameController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (invulnTimer > 0)
+        {
+            invulnTimer -= Time.deltaTime;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float xSpeed = Input.GetAxis("Horizontal");
+        float ySpeed = Input.GetAxis("Vertical");
+        Vector3 vel = new Vector3(xSpeed, 0, ySpeed) * SPEED_FACTOR;
+
+        body.AddForce(vel);
+    }
+
+    bool isPowered()
+    {
+        return invulnTimer > 0;
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "ghost") {
+            if (isPowered())
+            {
+                game.score += GHOST_SCORE;
+                Destroy(col.gameObject);
+            } else {
+                Debug.Log("test");
+                game.lives--;
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "pill")
+        {
+            game.score += PILL_SCORE;
+            Destroy(col.gameObject);
+        }
+        else if (col.gameObject.tag == "powerpill")
+        {
+            invulnTimer = POWER_PILL_TIMER;
+            Destroy(col.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/bt/BtController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BtController : MonoBehaviour
+{
+    private BtNode m_root;
+    private Blackboard m_blackboard;
+
+    // method to create the tree, sorry - no GUI for this we need to build it by hand
+    protected BtNode createTree() {
+        BtNode stickyTarget = new Selector(new Sequence(new IsTargeting("pill"), new Inverter(new IsClose(1))), new TargetRandom("pill"));
+        BtNode wonderToPill = new Sequence(stickyTarget, new TowardsTarget());
+        return wonderToPill;
+
+        //BtNode chasePlayer = new Sequence(new TargetPlayer("Player"), new IsClose(3), new TowardsTarget());
+        //return new Selector(chasePlayer, wonderToPill);
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+        if ( m_root == null) {
+            m_root = createTree();
+            m_blackboard = new Blackboard();
+            m_blackboard.owner = gameObject;
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+        NodeState result = m_root.evaluate(m_blackboard);
+        if ( result != NodeState.RUNNING ) {
+            m_root.reset();
+        }
+    }
+}
