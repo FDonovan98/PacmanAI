453e3375c1cfc0656d97e2928c0f273efbc48d81
HDonovan96 <harry.bd11@gmail.com>
2020-12-08 15:38:52 +0000

    Pinky now follows player at a slower speed than the player moves

---
+++ Assets/Scripts/bt/Behaviours/FollowPlayer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : BtController
+{
+    protected override BtNode createTree()
+    {
+        return new Sequence(new IsClose(100, "Player"), new TargetPlayer("Player"), new TowardsTarget());
+    }
+}
