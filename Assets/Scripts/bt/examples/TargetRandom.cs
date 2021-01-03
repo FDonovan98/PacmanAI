c789af97b34d13157cf10eda50e3bd62748f2a4f
HDonovan96 <harry.bd11@gmail.com>
2021-01-03 13:44:10 +0000

    Start setting up a forward weighting for TargetRandom

--- Assets/Scripts/bt/examples/TargetRandom.cs
+++ Assets/Scripts/bt/examples/TargetRandom.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class TargetRandom : BtNode
 {
     private MemoryType m_memoryType;
+    private bool m_weightedRandom;
 
-    public TargetRandom(MemoryType memoryType)
+    public TargetRandom(MemoryType memoryType, bool weightedRandom = false)
     {
         m_memoryType = memoryType;
+        m_weightedRandom = weightedRandom;
     }
 
     public override NodeState evaluate(Blackboard blackboard)
@@ -27,7 +29,28 @@ public class TargetRandom : BtNode
             return NodeState.FAILURE;
         }
 
-        blackboard.target = possibleItems[Random.Range(0, possibleItems.Count)];;
+        if (!m_weightedRandom)
+        {
+            blackboard.target = possibleItems[Random.Range(0, possibleItems.Count)];;
+        }
+        else
+        {
+            float[] targetAngles = new float[possibleItems.Count];
+            int i = 0;
+            foreach (AIRememberedItem element in blackboard.rememberedItems[(int)m_memoryType)
+            {
+                Vector3 dir = (element.position - blackboard.owner.transform.position).normalized;
+                targetAngles[i] = Vector3.Angle(blackboard.owner.transform.forward, dir);
+                i++;
+            }
+
+            List<AIRememberedItem> temp = new List<AIRememberedItem>();
+
+            for (i = 0; i < targetAngles.Length; i++)
+            {
+                
+            }
+        }
         m_nodeState = NodeState.SUCCESS;
         return m_nodeState;
     }
