47d89328106d48c8fea2a43208dfb8ef1166c2a0
HDonovan96 <harry.bd11@gmail.com>
2021-01-03 17:03:36 +0000

    Moved agent detection out of BtController to its own seperate script

---
+++ Assets/Scripts/Detection/AgentDetection.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SphereCollider))]
+public class AgentDetection : MonoBehaviour
+{
+    private BtController m_btController;
+    private SphereCollider sphereCollider;
+    private List<GameObject> itemsInRange = new List<GameObject>();
+    public float detectionRange = 7.0f;
+    public float rememberedObjectDisplacementTolerance = 1.0f;
+    // Remembered player count (index 0) needs to be at least 2 for directional analysis to work.
+    // See Assets\Editor\NamedArrayDrawer.cs for explanation of NamedArray attribute as it is NOT MY CODE.
+    [NamedArrayAttribute(new string[] {"Player", "Pill", "PowerPill"})]
+    public int[] rememberedItemCounts = new int[]
+    {
+        2, 30, 4
+    };
+    // Start is called before the first frame update
+    void Start()
+    {
+        try
+        {
+            m_btController = GetComponent<BtController>();
+            m_btController.m_blackboard = new Blackboard(gameObject, rememberedItemCounts, rememberedObjectDisplacementTolerance);
+        }
+        catch (System.Exception)
+        {
+            Debug.LogWarning(gameObject.name + " has no BtController attached");
+        }
+
+        sphereCollider = GetComponent<SphereCollider>();
+        sphereCollider.isTrigger = true;
+        sphereCollider.radius = detectionRange;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (GameObject element in itemsInRange)
+        {
+            CheckTagsToUpdateItems(element);
+        }
+    }
+
+        private void OnTriggerEnter(Collider other)
+    {
+        itemsInRange.Add(other.gameObject);
+
+        CheckTagsToUpdateItems(other.gameObject);
+
+    }
+
+    // Call local UpdateRememberedItems function dependant on other's tag.
+    private void CheckTagsToUpdateItems(GameObject other)
+    {
+        if (other.tag == "Player")
+        {
+            UpdateRememberedItems(MemoryType.Player, other);
+            return;
+        }
+
+        if (other.tag == "pill")
+        {
+            UpdateRememberedItems(MemoryType.Pill, other);
+            return;
+        }
+
+        if (other.tag == "powerpill")
+        {
+            UpdateRememberedItems(MemoryType.PowerPill, other);
+        }
+    }
+
+    // Checks if agent has line of sight to other.
+    private bool CheckForLOS(GameObject other)
+    {
+        Ray ray = new Ray(transform.position, other.transform.position - transform.position);
+        RaycastHit[] hits = Physics.RaycastAll(ray, Vector3.Distance(transform.position, other.transform.position));
+
+        if (hits.Length > 2)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    // Checks if agent has line of site to other and then calls the m_blackboard function to update remembered items.
+    void UpdateRememberedItems(MemoryType memoryType, GameObject other)
+    {
+        if (CheckForLOS(other))
+        {
+            m_btController.m_blackboard.UpdateRememberedItems(memoryType, other);
+        }
+    }
+
+    // Remove items from itemsInRange when they are outside of detection range.
+    private void OnTriggerExit(Collider other)
+    {
+        itemsInRange.Remove(other.gameObject);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        // Draw detection sphere
+        Gizmos.DrawWireSphere(transform.position, detectionRange);
+
+        // Draw remembered items.
+        int i = 0;
+        if (m_btController != null && m_btController.m_blackboard != null && m_btController.m_blackboard.rememberedItems.Length > 0)
+        {
+            foreach (AIRememberedItem[] array in m_btController.m_blackboard.rememberedItems)
+            {
+                switch (i)
+                {
+                    case (int)MemoryType.Player:
+                        Gizmos.color = Color.red;
+                        break;
+                    case (int)MemoryType.Pill:
+                        Gizmos.color = Color.green;
+                        break;
+                    case (int)MemoryType.PowerPill:
+                        Gizmos.color = Color.yellow;
+                        break;
+                    default:
+                        break;
+                }
+
+                if (array.Length > 0)
+                {
+                    foreach (AIRememberedItem element in array)
+                    {
+                        Gizmos.DrawCube(element.position, new Vector3(1.0f, 1.0f, 1.0f));
+                    }
+                }
+
+                i++;
+            }
+        }
+    
+        // Draw current target.
+        if (m_btController != null && m_btController.m_blackboard != null && m_btController.m_blackboard.target != null)
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawSphere(m_btController.m_blackboard.target.position, 1.0f);
+        }
+    }
+}
--- Assets/Scripts/bt/Behaviours/BtController.cs
+++ Assets/Scripts/bt/Behaviours/BtController.cs
@@ -9,27 +9,13 @@
 //               Assets\Scripts\bt\BtNode.cs
 // Description: Abstract class used as a base to build different ai behaviour scripts.
 
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(SphereCollider))]
 public abstract class BtController : MonoBehaviour
 {
     private BtNode m_root;
-    protected Blackboard m_blackboard;
-    private SphereCollider sphereCollider;
-    private List<GameObject> itemsInRange = new List<GameObject>();
-    public float detectionRange = 7.0f;
-    public float rememberedObjectDisplacementTolerance = 1.0f;
-    // Remembered player count (index 0) needs to be at least 2 for directional analysis to work.
-    // See Assets\Editor\NamedArrayDrawer.cs for explanation of NamedArray attribute as it is NOT MY CODE.
-    [NamedArrayAttribute(new string[] {"Player", "Pill", "PowerPill"})]
-    public int[] rememberedItemCounts = new int[]
-    {
-        2, 30, 4
-    };
+    public Blackboard m_blackboard;
 
     // method to create the tree, sorry - no GUI for this we need to build it by hand
     abstract protected BtNode createTree();
@@ -39,15 +25,8 @@ public abstract class BtController : MonoBehaviour
     {
         if (m_root == null)
         {
-            // Initialised with int array. 
-            // Each element corresponds to it's Enum MemoryType element.
-            m_blackboard = new Blackboard(gameObject, rememberedItemCounts, rememberedObjectDisplacementTolerance);
             m_root = createTree();
         }
-
-        sphereCollider = GetComponent<SphereCollider>();
-        sphereCollider.isTrigger = true;
-        sphereCollider.radius = detectionRange;
     }
 
     // Update is called once per frame
@@ -58,64 +37,6 @@ public abstract class BtController : MonoBehaviour
         {
             m_root.reset();
         }
-
-        foreach (GameObject element in itemsInRange)
-        {
-            CheckTagsToUpdateItems(element);
-        }
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        itemsInRange.Add(other.gameObject);
-
-        CheckTagsToUpdateItems(other.gameObject);
-
-    }
-
-    // Call local UpdateRememberedItems function dependant on other's tag.
-    private void CheckTagsToUpdateItems(GameObject other)
-    {
-        if (other.tag == "Player")
-        {
-            UpdateRememberedItems(MemoryType.Player, other);
-            return;
-        }
-
-        if (other.tag == "pill")
-        {
-            UpdateRememberedItems(MemoryType.Pill, other);
-            return;
-        }
-    }
-
-    // Checks if agent has line of sight to other.
-    private bool CheckForLOS(GameObject other)
-    {
-        Ray ray = new Ray(transform.position, other.transform.position - transform.position);
-        RaycastHit[] hits = Physics.RaycastAll(ray, Vector3.Distance(transform.position, other.transform.position));
-
-        if (hits.Length > 2)
-        {
-            return false;
-        }
-
-        return true;
-    }
-
-    // Checks if agent has line of site to other and then calls the blackboard function to update remembered items.
-    void UpdateRememberedItems(MemoryType memoryType, GameObject other)
-    {
-        if (CheckForLOS(other))
-        {
-            m_blackboard.UpdateRememberedItems(memoryType, other);
-        }
-    }
-
-    // Remove items from itemsInRange when they are outside of detection range.
-    private void OnTriggerExit(Collider other)
-    {
-        itemsInRange.Remove(other.gameObject);
     }
 
     virtual protected BtNode AwayFromPlayer(float fleeDistance, bool checkIfPowered = true)
@@ -131,51 +52,5 @@ public abstract class BtController : MonoBehaviour
     virtual protected BtNode MoveToPlayer(float huntRange)
     {
         return new Sequence(new IsClose(huntRange, MemoryType.Player), new TargetPlayer(MemoryType.Player), new TowardsTarget());
-    }
-
-    private void OnDrawGizmosSelected()
-    {
-        // Draw detection sphere
-        Gizmos.DrawWireSphere(transform.position, detectionRange);
-
-        // Draw remembered items.
-        int i = 0;
-        if (m_blackboard != null && m_blackboard.rememberedItems.Length > 0)
-        {
-            foreach (AIRememberedItem[] array in m_blackboard.rememberedItems)
-            {
-                switch (i)
-                {
-                    case (int)MemoryType.Player:
-                        Gizmos.color = Color.red;
-                        break;
-                    case (int)MemoryType.Pill:
-                        Gizmos.color = Color.green;
-                        break;
-                    case (int)MemoryType.PowerPill:
-                        Gizmos.color = Color.yellow;
-                        break;
-                    default:
-                        break;
-                }
-
-                if (array.Length > 0)
-                {
-                    foreach (AIRememberedItem element in array)
-                    {
-                        Gizmos.DrawCube(element.position, new Vector3(1.0f, 1.0f, 1.0f));
-                    }
-                }
-
-                i++;
-            }
-        }
-    
-        // Draw current target.
-        if (m_blackboard != null && m_blackboard.target != null)
-        {
-            Gizmos.color = Color.blue;
-            Gizmos.DrawSphere(m_blackboard.target.position, 1.0f);
-        }
     }
 }
